0477506c0d08e457c22d2cc01d4e321b83000280
shinnpuru <shinnpuru@users.noreply.github.com>
2025-04-23 14:10:39 +0000

    deploy: 82fda3fc583a31507380e55e69bc10dc2401a5f2

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3585488811"
+    serviceWorkerVersion: "2086818387"
   }
 });
