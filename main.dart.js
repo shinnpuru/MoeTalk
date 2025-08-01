80bb798334d724aa541576083e4dd8525f0f0fbb
shinnpuru <shinnpuru@users.noreply.github.com>
2025-08-01 16:10:25 +0000

    deploy: 7f8b125cacd9e08a54f00f2a1f487fc0c9d4b510

--- main.dart.js
+++ main.dart.js
@@ -31641,7 +31641,7 @@ $S:2}
 A.aJO.prototype={
 $1(a){var s=A.fG().b
 s=s==null?null:s.canvasKitBaseUrl
-return(s==null?"https://www.gstatic.com/flutter-canvaskit/39d6d6e699e51b2874210e14cddf1a22fb9524b2/":s)+a},
+return(s==null?"https://www.gstatic.com/flutter-canvaskit/ef0cd000916d64fa0c5d09cc809fa7ad244a5767/":s)+a},
 $S:117}
 A.aa6.prototype={
 fI(a,b){var s,r=this.a
